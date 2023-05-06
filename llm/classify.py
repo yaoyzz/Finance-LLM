5fa90393a5059923efaa694fd9d69e53091b7481
Yao <david.zhang201607@gmail.com>
2023-05-06 19:27:27 -0400

    update cohere

--- llm/classify.py
+++ llm/classify.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import json
-from cohere_classifier import *
+from cohere_classifier.cohere_classifier import *
 
 if __name__ == "__main__":
     with open("../api-keys.json", "r") as f:
         api_keys = json.load(f)
         print(f"Found keys for {', '.join(api_keys.keys())}")
+
+    CHClassifier = CohereClassifier(api_keys)
+    input = ["""the economic gets better, people buy the stocks"""]
+    rate = CHClassifier.get_ratings(input)
+    print(rate)
